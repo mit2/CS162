0427b61be239a01a562d312376bd2d0c95ae3f73
Charles Reiss <woggle@apache.org>
2015-06-01 20:05:18 -0700

    Add more dummy implementation to kvfs.c

--- hw3/kvfs.c
+++ hw3/kvfs.c
@@ -47,7 +47,8 @@ static int kvfs_getattr(const char *path, struct stat *stbuf)
     if (!strcmp(path, "/")) {
         return lstat(mountparent, stbuf);
     }
-    return 0;
+    /* Returning error for "file does not exist" */
+    return -ENOENT;
 }
 
 static int kvfs_truncate(const char *path, off_t size)
@@ -55,10 +56,42 @@ static int kvfs_truncate(const char *path, off_t size)
     return 0;
 }
 
+static int kvfs_open(const char *path, struct fuse_file_info *fi)
+{
+    return -ENOENT;
+}
+
+static int kvfs_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi)
+{
+    return -1;
+}
+
+static int kvfs_write(const char *path, const char *buf, size_t size, off_t offset,
+                       struct fuse_file_info *fi)
+{
+    return -EROFS;
+}
+
+static int kvfs_create(const char *path, mode_t mode, struct fuse_file_info *file_info)
+{
+    return -EROFS;
+}
+
+static int kvfs_unlink(const char *path)
+{
+    return -EROFS;
+}
+
 struct fuse_operations kvfs_oper = {
     .getattr    = kvfs_getattr,
     .truncate   = kvfs_truncate,
-    .init       = kvfs_init
+    .init       = kvfs_init,
+    .open       = kvfs_open,
+    .read       = kvfs_read,
+    .write      = kvfs_write,
+    .create     = kvfs_create,
+    .unlink     = kvfs_unlink
 };
 
 
