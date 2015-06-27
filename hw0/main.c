e1f22b3eb755a14dd37db72f5f5a1d8927b62915
Zhao Cong <zhaocong89@gmail.com>
2015-06-27 13:14:07 +0000

    Submitting hw0.

--- hw0/main.c
+++ hw0/main.c
@@ -4,7 +4,12 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %d\n",  0);
-    printf("process limit: %d\n", 0);
-    printf("max file descriptors: %d\n", 0);
+    getrlimit(RLIMIT_STACK, &lim);
+    printf("stack size: %lld\n",  (long long) lim.rlim_cur);
+    getrlimit(RLIMIT_NPROC, &lim);
+    printf("process limit: %lld\n", (long long) lim.rlim_cur);
+    getrlimit(RLIMIT_NOFILE, &lim);
+    printf("max file descriptors: %lld\n", (long long) lim.rlim_cur);
+
+    return 0;
 }
--- hw0/wc.c
+++ hw0/wc.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define BUFSIZE 1024
+
 void wc(FILE *ofile, FILE *infile, char *inname) {
+    int totalNewlines = 0;
+    int totalWords = 0;
+    int totalBytes = 0;
+    int inword = 0;
+    char buff[BUFSIZE];
+
+    if (inname)
+        infile = fopen(inname, "r");
+
+    if (!infile)
+        return;
+
+    while(fgets(buff, BUFSIZE, infile)) {
+        int i = 0;
+        for (i = 0; i < BUFSIZE && buff[i]; i++, totalBytes++) {
+            if (isspace(buff[i])) {
+                if (inword) {
+                    inword = 0;
+                    totalWords++;
+                }
+                if (buff[i] == '\n') {
+                    totalNewlines++;
+                }
+            }
+            else {
+                inword = 1;
+            }
+        }
+    }
+
+    printf("%d %d %d %s\n", totalNewlines, totalWords, totalBytes, inname);
+    fclose(infile);
+    fclose(ofile);
 }
 
 int main (int argc, char *argv[]) {
+    if (argc > 1) {
+        int i = 1;
+	for (i = 1; i < argc; i++) {
+       	    wc(stdout, NULL, argv[i]);
+        }
+    }
+    else {
+	wc(stdout, stdin, NULL);
+    }
     return 0;
 }
